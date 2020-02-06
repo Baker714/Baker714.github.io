b9bf456ba9d1a5a3f16180d4b2ed29e72d1c8cf3
Baker714 <baker714@github.com>
2020-02-05 22:15:12 -0500

    Adding resume

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SquareComponent } from './square/square.component';
 import { BoardComponent } from './board/board.component';
+import { ResumeDisplayComponent } from './resume-display/resume-display.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SquareComponent,
-    BoardComponent
+    BoardComponent,
+    ResumeDisplayComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/resume-display/resume-display.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resume-display',
+  templateUrl: './resume-display.component.html',
+  styleUrls: ['./resume-display.component.css']
+})
+export class ResumeDisplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
