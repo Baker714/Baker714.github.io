c09d417d345391a4a736d2b617d013aafced3684
Baker714 <baker714@github.com>
2020-02-09 22:35:04 -0500

    Changed a lot, added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { SquareComponent } from './square/square.component';
 import { BoardComponent } from './board/board.component';
 import { ResumeDisplayComponent } from './resume-display/resume-display.component';
+import { GamesComponent } from './games/games.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'resume', component: ResumeDisplayComponent},
+  {path: 'games', component: GamesComponent },
+  {path: '', redirectTo: '/home', pathMatch: 'full'}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     SquareComponent,
     BoardComponent,
-    ResumeDisplayComponent
+    ResumeDisplayComponent,
+    GamesComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
